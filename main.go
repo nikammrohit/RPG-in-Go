581707811ae310e1bf86e9ff90f7ac58be5f7911
Rohit Nikam <rohitnikam@rohits-mbp.lan>
2025-05-24 18:02:00 -0500

    reorganized code structure for scalability

--- main.go
+++ main.go
@@ -9,26 +9,30 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+type Sprite struct {
+	// Image and position variables for our player
+	Img  *ebiten.Image //spritesheet
+	X, Y float64
+}
 type Game struct {
 	// Values which are passed into Game
-	PlayerImage *ebiten.Image //uppercase P is a public field
-	X, Y        float64
+	player *Sprite // pointer to sprite struct
 }
 
 func (g *Game) Update() error {
 	// React to keypressed
 	SPEED := 2
 	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		g.X += float64(SPEED)
+		g.player.X += float64(SPEED)
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		g.X -= float64(SPEED)
+		g.player.X -= float64(SPEED)
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyUp) {
-		g.Y -= float64(SPEED)
+		g.player.Y -= float64(SPEED)
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyDown) {
-		g.Y += float64(SPEED)
+		g.player.Y += float64(SPEED)
 	}
 
 	return nil
@@ -40,10 +44,10 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	// Draw player
 	opts := ebiten.DrawImageOptions{}
-	opts.GeoM.Translate(g.X, g.Y) // Allows us to translate player for movement
+	opts.GeoM.Translate(g.player.X, g.player.Y) // Allows us to translate player for movement
 
 	screen.DrawImage(
-		g.PlayerImage.SubImage( // grab vector values for a single frame from spritesheet
+		g.player.Img.SubImage( // grab vector values for a single frame from spritesheet
 			image.Rect(0, 0, 13, 18), // start(inclusive), end(exclusive)
 		).(*ebiten.Image), // convs image.Image to *ebiten.Image which we want
 		&opts, // img options
@@ -65,7 +69,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if err := ebiten.RunGame(&Game{PlayerImage: playerImg, X: 100, Y: 100}); err != nil {
+	game := Game{
+		player: &Sprite{
+			Img: playerImg,
+			X:   50.0,
+			Y:   50.0,
+		},
+	}
+
+	if err := ebiten.RunGame(&game); err != nil {
 		log.Fatal(err)
 	}
 }
