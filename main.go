84c6effffea4efdd64a8fdaeec259f55278cca71
Rohit Nikam <rohitnikam@rohits-mbp.lan>
2025-05-25 12:01:09 -0500

    Created enemy character and implemented very basic player tracking algorithm. Additionally implemented health field for player.

--- main.go
+++ main.go
@@ -14,9 +14,21 @@ type Sprite struct {
 	Img  *ebiten.Image //spritesheet
 	X, Y float64
 }
+
+type Player struct {
+	*Sprite
+	Health uint
+}
+
+type Enemy struct {
+	*Sprite
+	followsPlayer bool
+}
+
 type Game struct {
 	// Values which are passed into Game
-	player *Sprite // pointer to sprite struct
+	player  *Player  // pointer to sprite struct
+	enemies []*Enemy // slice containing list of all sprites in game (except for player)
 }
 
 func (g *Game) Update() error {
@@ -33,6 +45,23 @@ func (g *Game) Update() error {
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyDown) {
 		g.player.Y += float64(SPEED)
+	}
+
+	// Algorithm to make enemies follow player
+	ENEMY_SPEED := 0.5
+	for _, sprite := range g.enemies {
+		if sprite.followsPlayer { // only sprites with followsPlayer := true property will follow player
+			if sprite.X < g.player.X {
+				sprite.X += float64(ENEMY_SPEED)
+			} else if sprite.X > g.player.X {
+				sprite.X -= float64(ENEMY_SPEED)
+			}
+			if sprite.Y < g.player.Y {
+				sprite.Y += float64(ENEMY_SPEED)
+			} else if sprite.Y > g.player.Y {
+				sprite.Y -= float64(ENEMY_SPEED)
+			}
+		}
 	}
 
 	return nil
@@ -52,6 +81,20 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		).(*ebiten.Image), // convs image.Image to *ebiten.Image which we want
 		&opts, // img options
 	)
+	opts.GeoM.Reset()
+
+	// Iterate through sprites[] slice and draw them
+	for _, sprite := range g.enemies {
+		// essentially copying draw code for player but for all sprites
+		opts.GeoM.Translate(sprite.X, sprite.Y)
+		screen.DrawImage(
+			sprite.Img.SubImage(
+				image.Rect(42, 24, 55, 40), //! Change coords for enemies
+			).(*ebiten.Image),
+			&opts,
+		)
+		opts.GeoM.Reset() // Reset translation back to (0, 0) for next entity
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -60,20 +103,59 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled) // Lets us resize our window
 
-	playerImg, _, err := ebitenutil.NewImageFromFile("./assets/Characters/_Source/Human/TRIMMED_walk.png")
+	// Load player spritesheet from file
+	playerImg, _, err := ebitenutil.NewImageFromFile("./assets/Characters/_Source/Human/TRIMMED_idle.png")
+	//! Insert enemyImg
+	if err != nil {
+		// Print err then exit game
+		log.Fatal(err)
+	}
+
+	// Load enemy spritesheet from file
+	skeletonImg, _, err := ebitenutil.NewImageFromFile("./assets/Characters/Skeleton/PNG/skeleton_idle_strip6.png")
+	//! Insert enemyImg
 	if err != nil {
 		// Print err then exit game
 		log.Fatal(err)
 	}
 
 	game := Game{
-		player: &Sprite{
-			Img: playerImg,
-			X:   50.0,
-			Y:   50.0,
+		player: &Player{
+			Sprite: &Sprite{
+				Img: playerImg,
+				X:   50.0,
+				Y:   50.0,
+			},
+			Health: 5,
+		},
+		// additional sprite slices of type Enemy struct
+		enemies: []*Enemy{
+			{
+				&Sprite{
+					Img: skeletonImg, //! Change source img
+					X:   70.0,
+					Y:   70.0,
+				},
+				false,
+			},
+			{
+				&Sprite{
+					Img: skeletonImg, //! Change source img
+					X:   90.0,
+					Y:   90.0,
+				},
+				true,
+			},
+			{
+				&Sprite{
+					Img: skeletonImg, //! Change source img
+					X:   110.0,
+					Y:   110.0,
+				},
+				true,
+			},
 		},
 	}
 
